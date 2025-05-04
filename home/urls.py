8887c42ecbb4985bea2479536663c558d1808917
anupamvishwakarma-goognu <anupam.kumar@goognu.com>
2025-05-05 00:15:39 +0530

    entry form

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from home import views
 urlpatterns = [
     path('', views.index, name='home'),
+    path('pateint-entry', views.entry_form, name='entry'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
+
+def entry_form(request):
+    return render(request, 'entry.html')
